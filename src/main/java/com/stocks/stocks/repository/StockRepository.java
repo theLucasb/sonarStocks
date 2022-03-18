059ce362fbdc713aef82d676026981612c871434
Lucas Rodrigo <lucasrodrigodes@gmail.com>
2022-03-18 17:19:39 -0300

    vamooo

--- src/main/java/com/stocks/stocks/repository/StockRepository.java
+++ src/main/java/com/stocks/stocks/repository/StockRepository.java
@@ -1,10 +1,5 @@
 package com.stocks.stocks.repository;
 
-// import java.util.List;
-// import java.util.Optional;
-
-// import javax.transaction.Transactional;
-
 import com.stocks.stocks.model.Stock;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,9 +7,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StockRepository extends JpaRepository<Stock, Long> {
-
-    // Optional<Stock> stockSymbol(String stockSymbol);
-
-    // List<Stock> findByName(String stock_name);
 
 }
